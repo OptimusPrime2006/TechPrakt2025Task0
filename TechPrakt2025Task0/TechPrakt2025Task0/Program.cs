b978814b0203078f9ebee9a742312f9813fd21a2
OptimusPrime2006 <vt231_mim@student.ztu.edu.ua>
2025-02-14 09:12:32 +0200

    gitignore

---
+++ TechPrakt2025Task0/TechPrakt2025Task0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Введіть перше число: ");
+if (double.TryParse(Console.ReadLine(), out double number1))
+{
+    Console.Write("Введіть друге число: ");
+    if (double.TryParse(Console.ReadLine(), out double number2))
+    {
+        double sum = number1 + number2;
+
+        Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює: {sum}");
+    }
+    else
+    {
+        Console.WriteLine("Некоректне друге число.");
+    }
+}
+else
+{
+    Console.WriteLine("Некоректне перше число.");
+}
